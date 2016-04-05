759ffc87fbff2f853a72cc1ba25265da900b79e6
Ian Hampson <inhampson@gmail.com>
2016-04-05 12:09:42 -0400

    Cleaning up client code 2.

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 "use strict";
-(init = () => {
+window.onload = () => {
   console.log("js loaded!");
   /// Code will be continued from here for the client... will look into browserfy or webpack to allow for gerneralizing and module patterning.
   var getElem = selector => {
@@ -84,4 +84,4 @@
 
       return false;
   });
-})();
+};
