f012659ebcc3b6ce7c774f1e9092844bf1ccee23
Ian Hampson <inhampson@gmail.com>
2016-04-05 10:48:46 -0400

    adding the new files I need.

---
+++ src/controllers/Account.js
@@ -0,0 +1,64 @@
+var models = require('../models');
+
+var Account = models.Account;
+
+var loginPage = (req,res) => {
+  res.render('login', {csrfToken: req.csrfToken()});
+};
+
+var signupPage = (req,res) => {
+  res.render('signup', {csrfToken: req.csrfToken()});
+};
+
+var logout = (req,res) => {
+  req.session.destroy();
+  res.redirect('/');
+};
+
+var login = (req,res) => {
+  if(!req.body.username || !req.body.pass){
+    return res.status(400).json({error: "All fields needed."});
+  }
+
+  Account.AccountModel.authenticate(req.body.username, req.body.pass, (err, account) => {
+      if(err || !account) return res.status(401).json({error: "Wrong user name or password."});
+
+      req.session.account = account.toAPI();
+      res.json({redirect: '/game'});
+  });
+};
+
+var signup = (req, res) => {
+  if(!req.body.username || !req.body.pass || !req.body.pass2)
+    return res.status(400).json({error: "All fields are required."});
+
+  if(req.body.pass !== req.body.pass2)
+    return res.status(400).json({error: "Passwords are not the same."});
+
+  Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
+    var accountData = {
+      username: req.body.username,
+      salt: salt,
+      password: hash,
+    }
+
+    var newAccount = new Account.AccountModel(accountData);
+
+    newAccount.save(err => {
+      if(err){
+        console.log(err);
+        return res.status(400).json({error: "There was an error."});
+      }
+
+      req.session.accout = newAccount.toAPI();
+      res.json({redirect: '/game'});
+    });
+
+  });
+};
+
+module.exports.loginPage = loginPage;
+module.exports.signupPage = signupPage
+module.exports.logout = logout;
+module.exports.login = login;
+module.exports.signup = signup;
---
+++ src/socketServ.js
@@ -0,0 +1,22 @@
+var init = io => {
+  var onJoin =socket => {
+    socket.on('join', data=> {
+      socket.join('room1');
+    });
+    socket.on('leaving', data=> {
+      // Remove players from room if they disconnect mid game.
+    });
+  };
+  var onMsg =socket => {
+    socket.on('update', data=> {
+      socket.to(data.room).emit("respond", {row:data.row, col:data.col, color:data.color});
+    });
+  };
+
+  io.sockets.on("connection", socket => {
+    onJoin(socket);
+    onMsg(socket);
+  });
+};
+
+module.exports.sockets = init;
