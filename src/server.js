9110d26b38be1f5ac603cbb0048249bbed60a286
Ian Hampson <inhampson@gmail.com>
2016-04-04 21:00:19 -0400

    Mistyped socket.io require.

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = socket.io(server);
+var io = require('socket.io')(server);
 
 var router = require('./router.js');
 var port = process.env.PORT || process.env.NODE_PORT || 3000;
