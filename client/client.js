33e8286943cd8e40814b6b79b5dc9fc8107b6fea
Ian Hampson <inhampson@gmail.com>
2016-04-12 00:18:49 -0400

    Fixing up my ajax calling.

--- client/client.js
+++ client/client.js
@@ -1,34 +1,34 @@
-"use strict";
 window.onload = () => {
+"use strict";
   console.log("js loaded!");
   /// Code will be continued from here for the client... will look into browserfy or webpack to allow for gerneralizing and module patterning.
   var getElem = selector => {
     return document.querySelector(selector);
   };
 
-  var handleError(message) => {
+  var handleError = (message) => {
       console.log(message);
       getElem("#errorMsg").innerHTML = message;
       getElem("#errDiv").style.display = block;
   };
 
-  function sendAjax(action, data) {
+  var sendAjax = (action, data) => {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', action);
-    xhr.send({
-        cache: false,
-        type: "POST",
-        url: action,
-        data: data,
-        dataType: "json",
-        success: function(result, status, xhr) {
-            window.location = result.redirect;
-        },
-        error: function(xhr, status, error) {
-            var messageObj = JSON.parse(xhr.responseText);
-            handleError(messageObj.error);
-        }
-    });
+    xhr.setRequestHeader('Content-type', 'json');
+    xhr.send(data);
+
+    xhr.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(xhr.responseText);
+        window.location = data.redirect;
+      }
+    };
+
+    xhr.onerror = () => {
+      var messageObj = JSON.parse(xhr.responseText);
+      handleError(messageObj.error);
+    };
       // $.ajax({
       //     cache: false,
       //     type: "POST",
@@ -46,12 +46,12 @@ window.onload = () => {
       //         handleError(messageObj.error);
       //     }
       // });
-  }
+  };
 
-  getElem("#signupSubmit") && getElem("#signupSubmit").addEventListener("click", function(e) {
+  getElem("#signupSubmit") && getElem("#signupSubmit").addEventListener("click", (e) => {
       e.preventDefault();
 
-      if(getElem("#user").val() == '' || getElem("#pass").val() == '' || getElem("#pass2").val() == '') {
+      if(getElem("#user").val() === '' || getElem("#pass").val() === '' || getElem("#pass2").val() === '') {
           document.alert("All fields are required");
           return false;
       }
@@ -69,9 +69,9 @@ window.onload = () => {
   getElem("#loginSubmit") && getElem("#loginSubmit").addEventListener("click", function(e) {
       e.preventDefault();
 
-      getElem("#domoMessage").animate({width:'hide'},350);
+      // getElem("#domoMessage").animate({width:'hide'},350);
 
-      if(getElem("#user").val() == '' || getElem("#pass").val() == '') {
+      if(getElem("#user").val() === '' || getElem("#pass").val() === '') {
           handleError("RAWR! Username or password is empty");
           return false;
       }
