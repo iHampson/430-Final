38e1ae724db7ef1d5bf81f70593099311dafd2c1
Ian Hampson <inhampson@gmail.com>
2016-05-01 15:51:53 -0400

    basic socket client code estabilshed.

--- client/game.js
+++ client/game.js
@@ -1,11 +1,24 @@
 window.onload = () => {
   "use strict";
 
-  console.log("game loaded");
+  var socket;
+
+  var init = () =>{
+    socket = io.connect();
+    initSockets();
+  };
+
+  var initSockets = () =>{
+    socket.emit("establish", {name: "unclear"});
+  };
+
 
   var hostCanvas = getElem("#host");
   hostCanvas.style.backgroundColor = '#77C223';
   // var hostCTX = hostCanvas.getContext('2d');
   // hostCTX.setBackground = '#77C223';
 
+
+  init();
+  console.log("game loaded");
 };
