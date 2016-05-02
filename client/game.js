f59fbeb7cb3f042724b9a57e4062adaf131e7ecb
Ian Hampson <inhampson@gmail.com>
2016-05-02 12:44:22 -0400

    base code for game in server, client, and view.

--- client/game.js
+++ client/game.js
@@ -2,22 +2,91 @@ window.onload = () => {
   "use strict";
 
   var socket;
+  var user = {};
+  var hostCanvas;
+  var hostMat;
+
+  var colorMap = { //'#'+Math.floor(Math.random()*16777215).toString(16);
+    1 : "#CD0CDD", 2 : "#5DE9C5",
+    3 : "#73BA21", 4 : "#ED4252",
+    5 : "#EB401A", 6 : "#B7BE2D",};
 
   var init = () =>{
     socket = io.connect();
+    hostCanvas = getElem("#host");
+    user.name = "#" + hostCanvas.getAttribute("data-name");
+
+    hostMat = new Array(3);
+    for(var k = 0; k<3; k++){
+      hostMat[k] = new Array(3);
+    }
+
+    for(var i = 0; i<3; i++){
+      for(var j = 0; j<3; j++){
+        hostMat[i][j] = Math.floor(Math.random()*6);
+      }
+    }
+
+    canvasSetup();
     initSockets();
+
+    window.beforeunload = () => {
+      socket.emit("leaving", user);
+    };
+
+  };
+
+  var canvasSetup = () => {
+    var hostCTX = hostCanvas.getContext('2d');
+
+    hostCTX.fillStyle = "#FFF";
+    hostCTX.fillRect(0,0,hostCanvas.width,hostCanvas.height);
+
+    hostCanvas.addEventListener("click", handleClick);
+    redraw(hostMat, "#host");
+  };
+
+  var handleClick = (e) => {
+    var x = e.offsetX;
+    var y = e.offsetY;
+
+    var hostCTX = hostCanvas.getContext('2d');
+    var setX = Math.floor(x/100);
+    var setY = Math.floor(y/100);
+    // console.log(`${setX},${setY}`);
+    hostMat[setY][setX] = hostMat[setY][setX] == 6 ? 1 : hostMat[setY][setX] + 1;
+
+    // hostCTX.fillStyle = "#FA00FF";
+    // hostCTX.fillRect(setX,setY,100,100);
+    redraw(hostMat, "#host");
+    hostCTX.fillStyle = "#FB0";
+    hostCTX.fillRect(x,y,10,10);
+    socket.emit('update', {drawMat : hostMat, user: user});
   };
 
   var initSockets = () =>{
-    socket.emit("establish", {name: "unclear"});
+    socket.emit("establish", {name: user.name});
+
+    socket.on('joined', data => {
+      var otherCan = document.createElement(canvas);
+      otherCan.setAttribute('id',data.sId); //Should change to be socketID
+    });
+
+    socket.on('drawThis', data =>{
+      redraw(data.colorMat,data.user.name);
+    });
   };
 
-
-  var hostCanvas = getElem("#host");
-  hostCanvas.style.backgroundColor = '#77C223';
-  // var hostCTX = hostCanvas.getContext('2d');
-  // hostCTX.setBackground = '#77C223';
-
+  var redraw = (colorMat,canvasId) => {
+    var drawCTX = getElem(canvasId).getContext('2d');
+    for(var i=0; i<3; i++){
+      for(var j=0; j<3; j++){
+        var color = colorMat[i][j];
+        drawCTX.fillStyle = colorMap[color];
+        drawCTX.fillRect(100*j,100*i,100,100);
+      }
+    }
+  };
 
   init();
   console.log("game loaded");
--- src/socketServ.js
+++ src/socketServ.js
@@ -1,22 +1,43 @@
-var sockets = io => {
-  var onJoin =socket => {
-    socket.on('join', data=> {
-      socket.join('room1');
+var init = io => {
+
+  var activeRoom = 'room1';
+
+  var onJoin = socket => {
+    socket.on('establish', data => {
+      console.log(data.name);
+
+      socket.join(activeRoom);
+      // console.log(socket);
+
+      data.sId = socket.id;
+      data.room = activeRoom;
+
+      socket.to(activeRoom).broadcast.emit('joined', data);
+      socket.emit('setRoom', {room: activeRoom});
     });
-    socket.on('leaving', data=> {
+
+    socket.on('leaving', data => {
       // Remove players from room if they disconnect mid game.
+      data.room ? socket.leave(data.room) : socket.emit('error', {msg:"no room passed"});
     });
+
   };
-  var onMsg =socket => {
+
+  var onMsg = socket => {
     socket.on('update', data=> {
       socket.to(data.room).emit("respond", {row:data.row, col:data.col, color:data.color});
     });
   };
 
   io.sockets.on("connection", socket => {
+    console.log('connection hit.');
     onJoin(socket);
     onMsg(socket);
   });
+
+  io.sockets.on("disconnect", socket => {
+    socket.leave('room1');
+  });
 };
 
-module.exports = sockets;
+module.exports.socketsSetup = init;
